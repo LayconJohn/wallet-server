75feea102c97c66281dcbd3abd88492f675c0323
layconjohn <layconjohn@gmail.com>
2023-08-18 12:59:41 -0300

    feat: get carts

--- src/controllers/CartsController.js
+++ src/controllers/CartsController.js
@@ -1,6 +1,16 @@
+import Cart from "../models/Cart.js";
+
 class CartsController {
     async index(req, res) {
-        return res.status(200).json({status: "ok"})
+        try {
+            const carts = await Cart.find();
+            return res.status(200).json(carts);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({error: "Internal server error"})
+        }
+
+        
     }
 }
 
